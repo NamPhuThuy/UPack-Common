2c98029abee60d8d83c57964dde5f45d58c263a1
NamPhuThuy <ttnam957@gmail.com>
2025-11-12 00:03:21 +0700

    update

--- Scripts/Common/Extension/ListExtension.cs
+++ Scripts/Common/Extension/ListExtension.cs
@@ -9,6 +9,8 @@ namespace NamPhuThuy.Common
     
     public static class ListExtensions
     {
+        #region Randomization
+
         /// <summary>
         /// Shuffles the list in place using the Fisher–Yates algorithm.
         /// </summary>
@@ -31,96 +33,17 @@ namespace NamPhuThuy.Common
             return copy;
         }
         
-        public static List<T> Clone<T>(this List<T> list)
-        {
-            List<T> temp = new List<T>();
-            foreach (var item in list)
-            {
-                temp.Add(item);
-            }
-            return temp;
-        }
-
-
         public static T RandomElement<T>(this List<T> list)
         {
             if (list.Count == 0) return default(T);
             return list[Random.Range(0, list.Count)];
         }
-        public static T GetElementClamp<T>(this List<T> list, int index)
-        {
-            if (list.Count == 0) return default(T);
-            return list[Mathf.Clamp(index, 0, list.Count - 1)];
-        }
 
-        public static int LastIndex<T>(this List<T> list)
-        {
-            return list.Count - 1;
-        }
-        public static T LastElement<T>(this List<T> list)
-        {
-            if (list.Count == 0) return default(T);
-            return list[list.Count - 1];
-        }
-        public static T FirstElement<T>(this List<T> list)
-        {
-            if (list.Count == 0) return default(T);
-            return list[0];
-        }
-        public static T PopLastElement<T>(this List<T> list)
-        {
-            if (list.Count == 0) return default(T);
-            T result = list[list.Count - 1];
-            list.RemoveAt(list.Count - 1);
-            return result;
-        }
-        public static T PopFirstElement<T>(this List<T> list)
-        {
-            if (list.Count == 0) return default(T);
-            T result = list[0];
-            list.RemoveAt(0);
-            return result;
-        }
         public static int RandomIndex<T>(this List<T> list)
         {
             return Random.Range(0, list.Count);
         }
-        public static T PopRandomElement<T>(this List<T> list)
-        {
-            if (list.Count == 0) return default(T);
-            int index = Random.Range(0, list.Count);
-            T result = list[index];
-            list.RemoveAt(index);
-            return result;
-        }
-
-        public static void Swap<T>(this List<T> list, int indexA, int indexB)
-        {
-            T tmp = list[indexA];
-            list[indexA] = list[indexB];
-            list[indexB] = tmp;
-        }
-
-        public static T Remove<T>(this List<T> list, Predicate<T> match)
-        {
-            int index = list.FindIndex(match);
-            if (index != -1)
-            {
-                T result = list[index];
-                list.RemoveAt(index);
-                return result;
-            }
-            return default(T);
-        }
-
-        public static bool Contain<T>(this List<T> list, Predicate<T> match)
-        {
-            foreach (T item in list)
-            {
-                if (match(item)) return true;
-            }
-            return false;
-        }
+        
         public static List<T> RandomElements<T>(this List<T> list, int count, bool allowRepeat = false)
         {
             List<T> cloner = list.Clone();
@@ -146,7 +69,7 @@ namespace NamPhuThuy.Common
             }
             return randList;
         }
-
+        
         public static List<T> Mix<T>(this List<T> list)
         {
             List<T> newList = new List<T>();
@@ -158,6 +81,162 @@ namespace NamPhuThuy.Common
             return newList;
         }
 
+        #endregion
+
+        #region Accessors
+
+        public static T GetElementClamp<T>(this List<T> list, int index)
+        {
+            if (list.Count == 0) return default(T);
+            return list[Mathf.Clamp(index, 0, list.Count - 1)];
+        }
+
+        public static int LastIndex<T>(this List<T> list)
+        {
+            return list.Count - 1;
+        }
+        
+        public static T LastElement<T>(this List<T> list)
+        {
+            if (list.Count == 0) return default(T);
+            return list[list.Count - 1];
+        }
+        public static T FirstElement<T>(this List<T> list)
+        {
+            if (list.Count == 0) return default(T);
+            return list[0];
+        }
+
+        #endregion
+
+        #region Stack-like Operations
+
+        public static T PopLastElement<T>(this List<T> list)
+        {
+            if (list.Count == 0) return default(T);
+            T result = list[list.Count - 1];
+            list.RemoveAt(list.Count - 1);
+            return result;
+        }
+        public static T PopFirstElement<T>(this List<T> list)
+        {
+            if (list.Count == 0) return default(T);
+            T result = list[0];
+            list.RemoveAt(0);
+            return result;
+        }
+
+        public static void Swap<T>(this List<T> list, int indexA, int indexB)
+        {
+            T tmp = list[indexA];
+            list[indexA] = list[indexB];
+            list[indexB] = tmp;
+        }
+        #endregion
+
+        #region Removal & Search
+
+        public static T Remove<T>(this List<T> list, Predicate<T> match)
+        {
+            int index = list.FindIndex(match);
+            if (index != -1)
+            {
+                T result = list[index];
+                list.RemoveAt(index);
+                return result;
+            }
+            return default(T);
+        }
+
+        public static bool Contain<T>(this List<T> list, Predicate<T> match)
+        {
+            foreach (T item in list)
+            {
+                if (match(item)) return true;
+            }
+            return false;
+        }
+
+        #endregion
+        
+        /// <summary>
+        /// Returns a random subset (distinct elements) of size <paramref name="count"/> from the source list.
+        /// If count >= list.Count returns a shallow copy of the whole list.
+        /// </summary>
+        private static List<T> RandomSubListDistinct<T>(this IList<T> list, int count)
+        {
+            if (list == null) throw new ArgumentNullException(nameof(list));
+            int n = list.Count;
+            if (count <= 0 || n == 0) return new List<T>();
+            if (count >= n) return new List<T>(list);
+
+            // Partial Fisher–Yates on indices
+            int[] indices = new int[n];
+            for (int i = 0; i < n; i++) indices[i] = i;
+
+            var result = new List<T>(count);
+            for (int i = 0; i < count; i++)
+            {
+                int r = UnityEngine.Random.Range(i, n);
+                (indices[i], indices[r]) = (indices[r], indices[i]);
+                result.Add(list[indices[i]]);
+            }
+            return result;
+        }
+        
+        /// <summary>
+        /// Returns a random sublist of size <paramref name="count"/>.
+        /// If allowRepeat == true elements may repeat; otherwise distinct.
+        /// </summary>
+        public static List<T> RandomSubList<T>(this IList<T> list, int count, bool allowRepeat = false)
+        {
+            if (!allowRepeat) return RandomSubListDistinct(list, count);
+
+            if (list == null) throw new ArgumentNullException(nameof(list));
+            int n = list.Count;
+            var result = new List<T>(Math.Max(0, count));
+            if (count <= 0 || n == 0) return result;
+
+            for (int i = 0; i < count; i++)
+            {
+                result.Add(list[UnityEngine.Random.Range(0, n)]);
+            }
+            return result;
+        }
+        
+        
+        
+        public static List<T> Clone<T>(this List<T> list)
+        {
+            List<T> temp = new List<T>();
+            foreach (var item in list)
+            {
+                temp.Add(item);
+            }
+            return temp;
+        }
+
+
+        
+        
+       
+       
+        
+        public static T PopRandomElement<T>(this List<T> list)
+        {
+            if (list.Count == 0) return default(T);
+            int index = Random.Range(0, list.Count);
+            T result = list[index];
+            list.RemoveAt(index);
+            return result;
+        }
+
+       
+
+      
+       
+
+       
         public static void Add<T>(this List<T> list, List<T> additionList)
         {
             foreach (var item in additionList)
--- Scripts/Common/Helpers/ExtensionVector3.cs
+++ Scripts/Common/Helpers/ExtensionVector3.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using NamPhuThuy.Common;
 using UnityEngine;
 
 namespace NamPhuThuy
